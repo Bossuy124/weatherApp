c5d22503b8716f22ec0671eb77eaec847285df99
Mrinall <bossguy124567@gmail.com>
2020-05-17 11:30:11 -0400

    Created latitude with class component and handeled errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Replaced the af with  a class component
-
 class App extends React.Component{
-  render(){
+  constructor(props) {         // Need a cosntructor
+    super(props);
+    // ONLY PUT EQUAL WHEN DECLARING STATE
+    this.state = { lat: null}; //Setting a object name LAT = ???
+
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position), // Success CallBack if the function works and gets position
-      (err) => console.log(err)
+      (position) => {
+        //Called setState to update the state of the latitude
+        // this is a CallBack function where we return the result of the state
+        this.setState({lat: position.coords.latitude});
+      },
+      (err) => {
+        this.setState({lat: err.message});
+      }
     );
-    return <div>Lat: </div>;
+
+  }
+  render(){
+
+    return <div>Lat: {this.state.lat} </div>; //How to reference a var inside the state
   }
 }
 
